a03584f753164885a423f018984de0d0c23ea6b1
Shashwath Bhaskar <110276216+zanzo2003@users.noreply.github.com>
2024-06-03 19:51:53 +0530

    Player class

--- Games/Space_Invader_/index.js
+++ Games/Space_Invader_/index.js
@@ -6,22 +6,42 @@ canvas.height = innerHeight
 
 class Player{
     constructor(){
-        this.position = {
-            x: 200,
-            y: 200
-        }
+    
         this.velocity = {
             x: 0,
             y: 0
         }
 
-        this.width = 100
-        this.height = 200
+        const image = new Image()
+        image.src = './img/spaceship.png'
+        image.onload = ()=>{
+            const scale = 0.15
+            this.width = image.width * scale
+            this.height = image.height * scale
+            this.image = image
+            this.position = {
+                x: canvas.width/2 - this.width/2,
+                y: canvas.height - this.height - 20
+            }
+        }
+
     }
 
     draw(){
-        c.fillStyle = "red";
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        //c.fillStyle = "red";
+       // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        if(this.image) c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
     }
 
 }
+
+const player = new Player()
+player.draw()
+
+function animate(){
+    requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.draw()
+}
+animate()
